296c70245b7555b5405008a0880205d9c592b0bb
ernestosperanza <ernestosperanza@gmail.com>
2020-04-16 20:12:56 -0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'donalo.yutki@gmail.com'
-EMAIL_HOST_PASSWORD = 'jlooxxlbrvwhextl'
+EMAIL_HOST_USER = '' #email
+EMAIL_HOST_PASSWORD = ''#Password
